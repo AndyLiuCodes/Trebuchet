0e821709a3a7fd74afd2a3126b437b96ab26e728
Andy Liu <32084972+AndyLiuCodes@users.noreply.github.com>
2023-02-25 17:42:50 -0800

    Add MySql Integration (#5)

--- server/index.js
+++ server/index.js
@@ -1,12 +1,30 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mysql from 'mysql';
 
 const app = express();
+dotenv.config();
 app.use(express.json());
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.listen(8080, () => console.log(`Server Port: 8080`));
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: process.env.DATABASE,
+});
+
+connection.connect();
+
+connection.query('SELECT * from Servers', function (err, res, fields) {
+  if (err) throw err;
+  console.log(res);
+});
+
+connection.end();
